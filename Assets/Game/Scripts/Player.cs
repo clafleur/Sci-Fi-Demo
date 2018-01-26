d4b82c014c253aef6030a73111c5c3f2e31e4be4
clafleur <clafleur@users.noreply.github.com>
2018-01-26 13:29:09 -0800

    Created a gun and player shoting logic
    Created a gun, updated the play script,

--- Assets/Game/Scripts/Player.cs
+++ Assets/Game/Scripts/Player.cs
@@ -7,6 +7,12 @@ public class Player : MonoBehaviour {
     private float speed = 3.5f;
     [SerializeField]
     private float gravity = 9.81f;
+    [SerializeField]
+    private GameObject muzzelFlash;
+    [SerializeField]
+    private GameObject hitMarkerPrefab;
+    [SerializeField]
+    private AudioSource weaponAudio;
 
     private CharacterController charController;
 	// Use this for initialization
@@ -14,17 +20,20 @@ public class Player : MonoBehaviour {
     {
         charController = GetComponent<CharacterController>();
         Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
 	}
 	
 	// Update is called once per frame
 	void Update ()
     {
-        CalculateMovement();
-
         if (Input.GetKeyDown(KeyCode.Escape))
         {
             Cursor.visible = true;
+            Cursor.lockState = CursorLockMode.None;
         }
+
+        CalculateMovement();
+        PlayerShot();
     }
 
     void CalculateMovement()
@@ -38,5 +47,30 @@ public class Player : MonoBehaviour {
 
         velocity = transform.transform.TransformDirection(velocity);
         charController.Move(velocity * Time.deltaTime);
+    }
+
+    void PlayerShot()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            muzzelFlash.SetActive(true);
+            if (!weaponAudio.isPlaying)
+            {
+                weaponAudio.Play();
+            }
+            Ray rayOrgin = Camera.main.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));
+            RaycastHit hitInfo;
+            if (Physics.Raycast(rayOrgin, out hitInfo))
+            {
+                Debug.Log("Raycast hit something " + hitInfo.transform.name);
+                GameObject hitMarker = Instantiate(hitMarkerPrefab, hitInfo.point, Quaternion.LookRotation(hitInfo.normal)) as GameObject;
+                Destroy(hitMarker, 1.0f);
+            }
+        }
+        else
+        {
+            muzzelFlash.SetActive(false);
+            weaponAudio.Stop();
+        }
     }
 }
